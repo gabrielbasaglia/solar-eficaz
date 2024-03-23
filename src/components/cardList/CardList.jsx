e0ef79e846055873efca966fb992389f4ed0b3ec
Gabriel Basaglia <garbaroots@hotmail.com>
2024-03-23 19:37:42 -0300

    correção de lógica CardList

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -2,41 +2,54 @@ import { Card } from "../card/Card";
 import { client } from "@/sanity/lib/client";
 import { Pagination } from "../pagination/Pagination";
 
-const POSTS_PER_PAGE = 4;
+const POST_PER_PAGE = 4;
+
+let lastPublishedAt = "";
+let lastId = "";
 
 async function buildPostQuery(page) {
-  return `*[_type == 'post'] [0...${POSTS_PER_PAGE}]{
-  title,
-  slug,
-  body,
-  publishedAt,
-  excerpt,
-  categories[] -> {
-    _id,
+  const skip = (page - 1) * POST_PER_PAGE;
+  return `*[_type == "post" && (
+    publishedAt > $lastPublishedAt
+    || (publishedAt == $lastPublishedAt && _id > $lastId)
+  )] | order(publishedAt desc) [${skip}...${skip + POST_PER_PAGE}] {
     title,
-  },
-  mainImage,
-  _id,
-} | order(publishedAt desc)`;
+    slug,
+    publishedAt,
+    excerpt,
+    categories[] -> {
+      _id,
+      title,
+    },
+    mainImage,
+    _id,
+  }`;
 }
 
 async function getPosts(page) {
   const query = await buildPostQuery(page);
-  const posts = await client.fetch(query);
+  const post = await client.fetch(query, { lastPublishedAt, lastId });
 
-  return posts;
+  if (post.length > 0) {
+    lastPublishedAt = post[post.length - 1].publishedAt;
+    lastId = post[post.length - 1]._id;
+  } else {
+    lastId = null;
+  }
+
+  return post;
 }
 
 export const CardList = async ({ page }) => {
-  const posts = await getPosts(page);
-  const hasPrev = POSTS_PER_PAGE * (page - 1) > 0;
-  const hasNext = posts.length === POSTS_PER_PAGE;
+  const post = await getPosts(page);
+  const hasPrev = POST_PER_PAGE * (page - 1) > 0;
+  const hasNext = post.length === POST_PER_PAGE;
 
   return (
     <div className="flex-5 mt-10 max-w-5xl">
       <h1 className="font-bold text-2xl capitalize font-pop">Mais recentes</h1>
       <div className="mt-6 max-w-5xl">
-        {posts?.map((post) => (
+        {post?.map((post) => (
           <Card post={post} key={post._id} />
         ))}
       </div>
